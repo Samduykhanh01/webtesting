0fa0b657a8ad7b3640e5494765ed243ac995f869
Thomas Vu <samduykhanh01@gmail.com>
2023-10-13 19:06:29 -0400

    Edited javascript.js

--- functions/javascript.js
+++ functions/javascript.js
@@ -6,3 +6,6 @@ console.log("multiply: " + multiply(2, 3));
 
 let capitalize = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
 console.log("capitalize: " + capitalize("heLLo"));
+
+let lastLetter = (str) => str.slice(-1);
+console.log("last letter of abcdefg is " + lastLetter("abcdefg"));
