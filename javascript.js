57dd573fba3d1f7d02c7ceeb3bc512276e266231
Thomas Vu <samduykhanh01@gmail.com>
2023-10-12 20:20:58 -0400

    Adding index.html and javascript.js PUSHING

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let add7 = (num) => num + 7;
+console.log("add7: "+ add7(3));
+
+let multiply = (a, b) => a * b;
+console.log("multiply: " + multiply(2, 3));
+
+let capitalize = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
+console.log("capitalize: " + capitalize("heLLo"));
